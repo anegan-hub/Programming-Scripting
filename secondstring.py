918bcec3401af27f4067437b3f72c208464905d4
anegan-hub <59984051+anegan-hub@users.noreply.github.com>
2020-02-02 19:23:02 +0000

    Create Topic 03 Variables

---
+++ secondstring.py
@@ -0,0 +1,9 @@
+# This programme will ask the user to input a string.
+# The output is every second letter, capitalised, in reverse order.
+
+InputString = (input("Enter a sentence here: "))
+
+InputString =  str. upper (InputString [::-2])
+
+print ("Every second character, in reverse order is: {}".format (InputString))
+
