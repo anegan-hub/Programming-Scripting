43071cbbfcd42e92ec7fd5ca3ee282ad0dcb501e
Andrea Egan <G00388212@gmit.ie>
2020-02-18 22:52:53 +0000

    Workings (incomplete)

---
+++ weekday.py
@@ -0,0 +1,33 @@
+# Andrea Egan 
+# This is a program that will output
+# whether or not today is a weekday.
+
+import datetime
+import calendar
+
+
+DT = datetime.datetime.today()
+
+print ("Today's Date: " , DT)
+
+DT.day
+print(DT.day)
+
+DT.month
+print(DT.month)
+
+DT.weekday()
+print(DT.weekday())
+
+
+
+
+
+
+
+
+
+
+
+
+
