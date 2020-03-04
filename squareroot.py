159bc6dc0db25d37a88cec9a660637d597fa38fe
Andrea Egan <G00388212@gmit.ie>
2020-03-04 23:52:53 +0000

    program created (incomplete)

---
+++ squareroot.py
@@ -0,0 +1,8 @@
+# Andrea Egan Task 6
+# This program will calculate the squareroot of a positive number
+
+P = float(input("Please input a positive number:"))
+
+X = (P * P)
+
+print ("the squareroot of" ,  P , "is" , X )
