9ad1fc52ba3cc62bdc49185651f8442ba15fd9b2
Andrea Egan <G00388212@gmit.ie>
2020-03-05 07:59:41 +0000

    Requires Squareroot Calculation

--- squareroot.py
+++ squareroot.py
@@ -3,8 +3,9 @@
 # to calculate the square root of a positive number
 
 def sqrt (x):
-    return(x * x)
+    return(x * x) # function to be updated to calculate squareroot 
 
 x = float(input("Please input a positive number: "))
 
 print ("the squareroot of" , x , "is" , sqrt(x) )
+
