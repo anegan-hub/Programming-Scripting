2d9360c8f125c8e10032a7d55fbbdbda130c2319
Andrea Egan <G00388212@gmit.ie>
2020-03-07 18:10:43 +0000

    Function updated and working

--- squareroot.py
+++ squareroot.py
@@ -1,11 +1,27 @@
 # Andrea Egan Task 6
-# This program includes the creation of a function
-# to calculate the square root of a positive number
+# This program calculates the approximate square root of a positive number
+# using a function based on Newtons method 
 
-def sqrt (x):
-    return(x * x) # function to be updated to calculate squareroot 
+def sqrt (x): # creation of the function 'sqrt'
+    est = x # estimated guess 
+    y = 0 
+    
+    while x > y: # while the input is greater than zero
+        est2 = (est - (est **2 - x) / (2 * est)) / 2
+
+        # new variable which muiltples a negative number with
+        # -1 to return the positive number
+        m = est2 - est 
+        if m < 0:   
+            m *= -1
+
+        est = est2
+        return est
 
 x = float(input("Please input a positive number: "))
 
-print ("the squareroot of" , x , "is" , sqrt(x) )
+print ("the squareroot of" , x , "is approx" , sqrt (x))
+
+# Reference https://www.youtube.com/watch?v=tUFzOLDuvaE
+# https://www.oreilly.com/programming/free/files/a-whirlwind-tour-of-python.pdf
 
