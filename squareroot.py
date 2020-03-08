34a349b689289004b506b04061905746eac52ed1
Andrea Egan <G00388212@gmit.ie>
2020-03-08 01:38:56 +0000

    Function simplified & working

--- squareroot.py
+++ squareroot.py
@@ -7,14 +7,7 @@ def sqrt (x): # creation of the function 'sqrt'
     y = 0 
     
     while x > y: # while the input is greater than zero
-        est2 = (est - (est **2 - x) / (2 * est)) / 2
-
-        # new variable which muiltples a negative number with
-        # -1 to return the positive number
-        m = est2 - est 
-        if m < 0:   
-            m *= -1
-
+        est2 = (x**.5)
         est = est2
         return est
 
@@ -24,4 +17,5 @@ print ("the squareroot of" , x , "is approx" , sqrt (x))
 
 # Reference https://www.youtube.com/watch?v=tUFzOLDuvaE
 # https://www.oreilly.com/programming/free/files/a-whirlwind-tour-of-python.pdf
-
+# https://en.wikipedia.org/wiki/Square_root
+# https://www.youtube.com/watch?v=UGac_KqFrn0
