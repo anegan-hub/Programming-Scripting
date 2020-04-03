db7be1fd979f0795d83bed75c8d30165a1686d78
Andrea Egan <G00388212@gmit.ie>
2020-04-03 20:27:34 +0100

    Function plot

---
+++ ipython_log.py
@@ -0,0 +1,29 @@
+# IPython log file
+
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.linspace(0, 4,)
+g = x **2
+h = x **3
+plt.plot(x, g, 'b.', label = "g(x)=x2")
+plt.plot(x, h, 'r.', label = "h(x)=x3")
+plt.legend()
+plt.title("Function Plot")
+plt.xlabel("f(x)=x")
+plt.ylabel("Y")
+plt.show()
+plt.show()
+plt.plot()
+plt show()
+plt.show()
+plt.plot(x, g, 'b.', label = "g(x)=x2")
+get_ipython().run_line_magic('logstart', '')
+plt.plot(x, h, 'r.', label = "h(x)=x3")
+plt.title("Function Plot")
+plt.xlabel("f(x)=x")
+plt.legend()
+x = np.linspace(0, 4,)
+g = x **2
+h = x **3
+x = np.linspace(0, 12,)
+exit()
