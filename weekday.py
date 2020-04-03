721d21d1b3d6da7eed9a9aa9e2a31d0e7c50386b
Andrea Egan <G00388212@gmit.ie>
2020-04-03 19:08:56 +0100

    Cleaned up comments, reference added

--- weekday.py
+++ weekday.py
@@ -1,13 +1,18 @@
 # Andrea Egan Task 5
 # This is a program that will output
 # whether or not today is a weekday.
+# To create the program, first you must import "datetime"
+# This module supplies the dates and times.
 
 import datetime
 
 DT = datetime.datetime.now() 
-days = (DT.isoweekday()) # weeks start with 1 
+days = (DT.isoweekday()) # Returns the day of the week where Monday is 1
 
 if days <= 5:
     print ("Yes unfortunately today is a weekday")    
 else:
     print ("Yah! It is the weekend")
+
+
+# Reference https://jakevdp.github.io/WhirlwindTourOfPython
